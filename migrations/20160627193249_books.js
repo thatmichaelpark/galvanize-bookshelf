6a02e3bd2903660de38209b3c8f6bdac5eac027b
Michael Park <thatmichaelpark@gmail.com>
2016-06-28 09:56:32 -0700

    add index for real

--- migrations/20160627193249_books.js
+++ migrations/20160627193249_books.js
@@ -3,7 +3,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('books', (table) => {
     table.increments();
-    table.integer('author_id').notNullable().references('id').inTable('authors').onDelete('CASCADE').index;
+    table.integer('author_id').notNullable().references('id').inTable('authors').onDelete('CASCADE').index();
     table.string('title').notNullable().defaultTo('');
     table.string('genre').notNullable().defaultTo('');
     table.text('description').notNullable().defaultTo('');
