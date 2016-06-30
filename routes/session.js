6e5e911d46193c01c5d30694870e3254a6c06695
Michael Park <thatmichaelpark@gmail.com>
2016-06-30 15:51:24 -0700

    add session routes

--- routes/session.js
+++ routes/session.js
@@ -2,7 +2,40 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
 
-// YOUR CODE HERE
+router.post('/session', (req, res, next) => {
+  knex('users')
+  .where('email', req.body.email)
+  .first()
+  .then((user) => {
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    const hashed_password = user.hashed_password;
+
+    bcrypt.compare(req.body.password, hashed_password, (err, isMatch) => {
+      if (err) {
+        return next(err);
+      }
+      if (!isMatch) {
+        return res.sendStatus(401);
+      }
+      req.session.user = user;
+      res.cookie('loggedIn', true);
+      res.sendStatus(200);
+    })
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+router.delete('/session', (req, res, next) => {
+  req.session = null;
+  res.clearCookie('loggedIn');
+  res.sendStatus(200);
+})
 
 module.exports = router;
