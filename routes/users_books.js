f02caa795ffc30e9b43422cc7e63c50502ef39f8
Michael Park <thatmichaelpark@gmail.com>
2016-06-30 16:32:04 -0700

    save in advance of pull from upstream

--- routes/users_books.js
+++ routes/users_books.js
@@ -2,7 +2,42 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+router.get('/users/books', (req, res, next) => {
+  knex('books')
+  .innerJoin('users_books', 'users_books.book_id', 'books.id')
+  .where('users_books.user_id', req.session.user.id)
+  .then((books) => {
+    res.send(books);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+router.get('/users/books/:bookId', (req, res, next) => {
+  knex('books')
+  .innerJoin('users_books', 'users_books.book_id', 'books.id')
+  .where('users_books.user_id', req.session.user.id)
+  .where('books.id', req.params.bookId)
+  .then((books) => {
+    res.send(books);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+router.post('/users/books/:bookId', (req, res, next) => {
+  knex('users_books')
+  .insert({user_id: req.session.user.id, book_id: req.params.bookId}, '*')
+  .then((results) => {
+    res.send(results[0]);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
 
 module.exports = router;
